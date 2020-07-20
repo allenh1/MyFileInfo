f84273e47473b7026854709b7879bfe5c8f31c63
Hunter L. Allen <hunter.allen@ghostrobotics.io>
2020-07-20 10:56:36 -0400

    Change to use CMake (#1)
    Signed-off-by: Hunter L. Allen <hunter.allen@ghostrobotics.io>

---
+++ src/main.cc
@@ -0,0 +1,16 @@
+#include <QtGui>
+#include <QFile>
+
+#include <MyFileInfo/MainWindow.h>
+
+int main(int argc, char ** argv)
+{
+    QApplication app(argc, argv);
+    MainWindow w(argc,argv);
+    w.show();
+    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+    //for(;;)
+    //	;
+    //system("pause");
+    return app.exec();
+}
